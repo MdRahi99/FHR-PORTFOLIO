eebccd5ef0a38db6142664bbffdea242e9f156f7
MdRahi99 <mdrahi181199@gmail.com>
2024-08-01 20:11:04 +0600

    add resume image

--- app/components/Resume/Resume.jsx
+++ app/components/Resume/Resume.jsx
@@ -1,21 +1,27 @@
+import Image from "next/image";
 import React from "react";
+import resume from "../../../public/images/resume.jpg";
+import { FiDownload } from "react-icons/fi";
+import Link from "next/link";
 
 const Resume = () => {
+  const url = 'https://drive.google.com/file/d/1yE2FQmp5aFW-FlgNpFtqx2mox5ccx9AE/view?usp=sharing'
   return (
     <div className="flex flex-col gap-4">
-      <div className="">
-        <h1 className="text-2xl lg:text-3xl font-bold mb-4">Resume</h1>
-        <div className="w-20 h-1 bg-hover"></div>
-      </div>
-      <div className="flex flex-col bg-primary text-white">
-        <div className="bg-primary p-1 rounded-lg shadow-lg">
-          <embed
-            src="/resume.pdf"
-            type="application/pdf"
-            width="800"
-            height="450"
-          />
+      <div>
+        <div className="flex items-center justify-between">
+          <div>
+            <h1 className="text-2xl lg:text-3xl font-bold mb-4">Resume</h1>
+            <div className="w-20 h-1 bg-hover"></div>
+          </div>
+          <Link href={url} target="_blank" className="flex items-center gap-1 px-4 py-2 rounded-md hover:bg-primary hover:text-hover cursor-pointer bg-secondary">
+            <FiDownload className="text-xl" />{" "}
+            View
+          </Link>
         </div>
+      </div>
+      <div className="flex justify-center">
+        <Image src={resume} alt="Resume" className="w-full h-auto" />
       </div>
     </div>
   );
