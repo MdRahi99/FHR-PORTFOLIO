71194bf81dc6f814236f580cb065287f4905ffa5
MdRahi99 <mdrahi181199@gmail.com>
2024-08-09 02:17:15 +0600

    add mobile menu

--- app/components/Menu/MobileMenu.jsx
+++ app/components/Menu/MobileMenu.jsx
@@ -1,35 +1,25 @@
-"use client";
-
+'use client'
 import sidebarItems from "@/public/data/sidebar";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import React from "react";
+import { FiX } from "react-icons/fi";
 
-const MobileMenu = () => {
-  const path = usePathname();
-
+const MobileMenu = ({ isOpen, toggleDrawer }) => {
   return (
-    <div className="px-4 py-2 bg-secondary flex flex-col gap-8">
-      <div className="flex flex-row justify-between gap-3 px-4">
-        {sidebarItems?.map((item, index) => {
-          const isActive = path === item.route;
-          return (
-            <Link
-              href={item.route}
-              key={index}
-              className={`group flex items-center gap-3 py-2 px-3 rounded-md transition-colors duration-300 ${
-                isActive
-                  ? "font-semibold text-hover"
-                  : "text-primary hover:text-secondary"
-              }`}
-            >
-              <span className="text-xl transition-transform duration-300 group-hover:scale-105">
-                {item.icon}
-              </span>
+    <div className={`fixed inset-0 z-50 transition-transform duration-300 ${isOpen ? 'translate-x-0' : 'translate-x-full'} bg-primary bg-opacity-75`}>
+      <div className="fixed top-0 right-0 w-2/3 h-full bg-primary shadow-lg p-5">
+        <button onClick={toggleDrawer} className="text-primary text-2xl mb-6">
+          <FiX />
+        </button>
+        <nav className="space-y-6">
+          {sidebarItems.map((item) => (
+            <Link key={item?.name} href={item?.route} className="flex items-center text-primary font-medium hover:text-secondary transition-colors duration-200">
+              {item?.icon}
+              <span className="ml-3">{item?.name}</span>
             </Link>
-          );
-        })}
+          ))}
+        </nav>
       </div>
+      <div className="fixed inset-0 z-40" onClick={toggleDrawer}></div>
     </div>
   );
 };
--- app/components/Menu/TopMenu.jsx
+++ app/components/Menu/TopMenu.jsx
@@ -1,15 +1,30 @@
 'use client'
-import { BsFiletypePdf } from "react-icons/bs";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import { BsFiletypePdf } from "react-icons/bs";
+import { FiMenu } from "react-icons/fi";
+import MobileMenu from "./MobileMenu";
 
 const TopMenu = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
   return (
     <div className="flex items-center justify-between">
       <Link href='/' className="p-2 rounded-tl-xl rounded-br-xl border-[3px] w-11 h-11 border-hover flex items-center justify-center">
         <h1 className="font-medium text-hover text-2xl">R</h1>
       </Link>
-      
+
+      <button onClick={toggleDrawer} className="lg:hidden p-2">
+        <FiMenu className="text-2xl" />
+      </button>
+
+      {/* Drawer Menu */}
+      <MobileMenu isOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
+
       <Link
         href='/resume'
         className="hidden lg:flex w-32 items-center justify-center py-1.5 rounded-md border-2 hover:border-primary hover:text-primary border-hover text-hover transition-all duration-300 ease-in-out"
