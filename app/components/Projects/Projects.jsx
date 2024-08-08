1611557e15204efbec492cd8f110dba2af6a7126
MdRahi99 <mdrahi181199@gmail.com>
2024-08-08 13:25:19 +0600

    fix

--- app/components/Projects/Projects.jsx
+++ app/components/Projects/Projects.jsx
@@ -44,9 +44,9 @@ const Projects = ({ projectsData, categoryData }) => {
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         {filteredProjects?.map((content) => (
-          <div key={content?._id} className="mb-2 flex flex-col gap-2 bg-secondary p-4 rounded-lg">
-            <h1>{content?.title}</h1>
+          <div key={content?._id} className="mb-2 flex flex-col gap-4 bg-secondary pb-4 rounded-b-lg">
             <ProductCard content={content} />
+            <h1 className="text-center">{content?.title}</h1>
           </div>
         ))}
       </div>
